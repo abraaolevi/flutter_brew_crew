745e99b70e5ed9759a075fd2fd0c133ac8f89744
Abraao Levi <abr4ao.levi@gmail.com>
2020-01-04 21:40:56 -0300

    Add new screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_brew_crew/screens/authenticate/authenticate.dart';
+import 'package:flutter_brew_crew/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
