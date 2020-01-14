688eb7a40f2c68449fb44a1fed56486eef582734
Abraão Levi <abr4ao.levi@gmail.com>
2020-01-14 10:16:19 -0300

    Add Database Service When register a user create a default entry to firestore

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_brew_crew/models/user.dart';
+import 'package:flutter_brew_crew/services/database_service.dart';
 
 class AuthService {
 
@@ -39,6 +40,10 @@ class AuthService {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
+
+      // create a new document for the user with uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
+
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
